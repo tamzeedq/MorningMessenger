fefd1054d56d0419e21a1cef631159c11eef6ea7
tamzeedq <74509993+tamzeedq@users.noreply.github.com>
2024-05-02 21:17:23 -0700

    set up Twilio

---
+++ text.py
@@ -0,0 +1,17 @@
+# Twilio Phone number: +13346058307
+import os
+from twilio.rest import Client
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+# twilio_phone = os.environ['TWILIO_PHONE_NUMBER']
+# personal_phone = os.environ['PERSONAL_PHONE_NUMBER']
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+  from_=twilio_phone,
+  body='Hello world2',
+  to=personal_phone
+)
+
+print(message.sid)
